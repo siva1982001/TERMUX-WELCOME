b11a027b9c1d62d8b6560e58dc8344fe35ca9e42
siva1982001 <72336823+siva1982001@users.noreply.github.com>
2020-10-26 14:36:12 +0530

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Update Script for PowerLevel10K v2.1-Stable
-# Script created by @certified_youtuber
+# Script created by @sivaprasad123
 
 
 dependencies() {
@@ -16,7 +16,7 @@ printf "\n \e[1;92mUpdating \e[1;94mTERMUX-WELCOME\e[1;92m ...\n\n"
 sleep 1.5
 cd ..
 rm -rf TERMUX-WELCOME
-git clone https://github.com/R0OTBYTE/TERMUX-WELCOME
+git clone https://github.com/siva1982001/TERMUX-WELCOME
 cd TERMUX-WELCOME
 printf "\n\e[1;92mRestarting ...\n\e[0m"
 bash install.sh
